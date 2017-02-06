97edc69939f196b181e4672d18ccd19496cc1a87
SARATH SRINIVAS S <sarathsrinivas07@gmail.com>
2017-02-06 22:14:43 +0530

    Separate gauss

---
+++ gauss.h
@@ -0,0 +1,3 @@
+#include <stddef.h>
+int gauss_grid_create(size_t size, double* x, double* w, double xmin, double xmax);
+int test_gauss_grid_create(void);
---
+++ gauss_internal.h
@@ -0,0 +1,7 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <float.h>
+
+void gauss_legendre_tbl(size_t n, double *x, double *w, double eps);
