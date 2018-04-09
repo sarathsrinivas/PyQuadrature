d0802bdbd4c3f7c90fc54beb5016585a3c477cc3
sarath srinivas s <srinix@protonmail.com>
2018-04-09 18:43:18 +0530

    Removed lebedev. lebedev is in lib_sph_harmonics.

--- lib_quadrature.h
+++ lib_quadrature.h
@@ -1,5 +1,3 @@
 #include <stddef.h>
 int gauss_grid_create(size_t size, double* x, double* w, double xmin, double xmax);
 double test_gauss_grid_create(unsigned long n, int tfun);
-int get_lebedev_grid(double **theta, double **phi, double **wleb, int nleb, unsigned long *ngrid, char* path);	
-double test_get_lebedev_grid(int leb, int tfun);
